7bb90c3c5c6c54b29f4d21e9d00ec34dab0fbac1
pkastanidiss <pkastanidiss@gmail.com>
2025-07-22 19:24:57 +0300

    feat: add addition class

--- src/gr/aueb/cf/helloapp/AddApp.java
+++ src/gr/aueb/cf/helloapp/AddApp.java
@@ -1,4 +1,12 @@
 package gr.aueb.cf.helloapp;
 
 public class AddApp {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static int add(int a, int b) {
+        return a + b;
+    }
 }
